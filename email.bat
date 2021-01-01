7bd195371156c935364fc6c27f44289d24686914
Jensen Hu <jensennhu@gmail.com>
2021-01-01 07:10:12 -0800

    updating data

---
+++ email.bat
@@ -0,0 +1,4 @@
+@echo off
+"C:\Program Files\R\R-4.0.3\bin\R.exe" CMD BATCH "C:\Users\jense\Desktop\Projects\covid_email_report_production\test_automation\send_email.R"
+CD "C:\Users\jense\Desktop\Projects\covid_email_report_production\test_automation\docs"
+CALL "C:\Users\jense\Desktop\Projects\covid_email_report_production\test_automation\docs\update.bat"
