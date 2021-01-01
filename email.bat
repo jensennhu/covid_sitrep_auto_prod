4b3b4e0ebb91acf8c1e1ed1c87f3083e0871cae0
Jensen Hu <jensennhu@gmail.com>
2020-12-31 16:16:18 -0800

    update

--- email.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"C:\Program Files\R\R-4.0.3\bin\R.exe" CMD BATCH "C:\Users\jense\Desktop\Projects\covid_email_report_staging\test_automation\send_email.R"
-CALL "C:\Users\jense\Desktop\Projects\covid_email_report_staging\test_automation\docs\update.bat"
