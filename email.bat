fd906c34def548e4b5278ac676932aeffa9c2e44
Jensen Hu <jensennhu@gmail.com>
2021-01-19 21:14:47 -0500

    updating data

--- email.bat
+++ email.bat
@@ -1,4 +1,7 @@
 @echo off
+"C:\Program Files\R\R-4.0.3\bin\R.exe" CMD BATCH "C:\Users\jense\Desktop\Projects\covid_email_report_production\test_automation\master_email.Rmd"
 "C:\Program Files\R\R-4.0.3\bin\R.exe" CMD BATCH "C:\Users\jense\Desktop\Projects\covid_email_report_production\test_automation\send_email.R"
 CD "C:\Users\jense\Desktop\Projects\covid_email_report_production\test_automation\docs"
 CALL "C:\Users\jense\Desktop\Projects\covid_email_report_production\test_automation\docs\update.bat"
+CD "C:\Users\jense\Desktop\Projects\covid_email_report_production\test_automation\master_shiny"
+CALL "C:\Users\jense\Desktop\Projects\covid_email_report_production\test_automation\master_shiny\update.bat"
